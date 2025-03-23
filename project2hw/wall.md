32acca20396deb7127df2d9316cfd64a81a0faa3
Max Rivas <Rivas.max11@csu.fullerton.edu>
2025-03-23 10:42:03 -0700

    added files for project 2

---
+++ project2hw/wall.md
@@ -0,0 +1,14 @@
+# This is for the wall.py file located in this same directory!
+Made by Max Rivas, partnered with Marco Macias for our CPSC 335 Project 2
+# How to Run:
+1. It is simple, download the wall.py file, and use any instance of a terminal, including VSCode's
+
+2. In the proper directory of wherever the file is downloaded, enter "python3 wall.py"
+
+3. The project takes an input of a m*n matrix, and displays three kinds of spaces:
+    -An "inf" value, where there are citizens fleeing to safe zones
+    -A "-1" area, filled with titans that citizens cannot bypass
+    -A "0" zone, where citizens can take refuge in
+The output matrix will display the same m*n matrix, but the spaces occupied with how many movements it would take for every "inf" to reach a "0"
+
+4. You can modify the test case by doing "code wall.py" and find the test case near the bottom of the page
---
+++ project2hw/wall.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+def find_nearest_stronghold(cities, x, y):
+  m, n = len(cities), len(cities[0])
+  q = deque()
+  q.append((x, y, 0))
+  while q:
+    i, j, distance = q.popleft()
+    if cities[i][j] == 0:
+      return distance
+    for dx, dy in directions:
+      new_i, new_j = i + dx, j + dy
+      if new_i not in range(m) or new_j not in range(n) or cities[new_i][new_j] == -1:
+        continue
+      q.append((new_i, new_j, distance + 1))
+  return -1
+
+
+def nearest_cities(cities):
+  m, n = len(cities), len(cities[0])
+  for i in range(m):
+    for j in range(n):
+      if cities[i][j] == float('inf'):
+        cities[i][j] = find_nearest_stronghold(cities, i, j)
+
+def test():
+  test_input = [
+    [0, float('inf'), float('inf')],
+    [-1, -1, float('inf')],
+    [-1, -1, float('inf')],
+  ]
+  for r in test_input:
+    print(r)
+    print("\n")
+
+  print("Output:\n")
+  nearest_cities(test_input)
+  for r in test_input:
+    print(r)
+
+if __name__ == '__main__':
+  test()
